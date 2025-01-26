15552671f33f86830ea4c9833051a6f472ca0860
ajmosley345 <ajmosley345@gmail.com>
2025-01-26 11:02:26 -0500

    Fix cloud-config

--- terraform/main.tf
+++ terraform/main.tf
@@ -43,30 +43,41 @@ resource "hcloud_server" "am_static_site" {
   user_data = <<EOT
 #cloud-config
 # Creates the user ansible for configuration with ansible playbooks
-name: ansible
-groups: users, admin
-sudo: ALL=(ALL) NOPASSWD:ALL
-shell: /bin/bash
-ssh_authorized_keys:
-  - ${var.ansible_user_ssh_key}
+users:
+- name: ansible
+  groups: users, admin
+  sudo: ALL=(ALL) NOPASSWD:ALL
+  shell: /bin/bash
+  ssh_authorized_keys:
+    - ${var.ansible_user_ssh_key}
 
 # Writes the ssh hardening config to a separate file for better management
-write_files:
-  path: /etc/ssh/sshd_config.d/hardening.conf
-  content: |
-    PermitRootLogin no
-    PasswordAuthentication no
-    KbdInteractiveAuthentication no
-    ChallengeResponseAuthentication no
-    MaxAuthTries 2
-    AllowTcpForwarding no
-    X11Forwarding no
-    AllowAgentForwarding no
-    AuthorizedKeysFile .ssh/authorized_keys
-    AllowUsers ${var.personal_user} ansible
+# write_files:
+#   path: /etc/ssh/sshd_config.d/hardening.conf
+#   content: |
+#     PermitRootLogin no
+#     PasswordAuthentication no
+#     KbdInteractiveAuthentication no
+#     ChallengeResponseAuthentication no
+#     MaxAuthTries 2
+#     AllowTcpForwarding no
+#     X11Forwarding no
+#     AllowAgentForwarding no
+#     AuthorizedKeysFile .ssh/authorized_keys
+#     AllowUsers ${var.personal_user} ansible
 
 # Webhook to call bootstrap-server workflow
 runcmd:
+  - sed -i -e '/^\(#\|\)PermitRootLogin/s/^.*$/PermitRootLogin no/' /etc/ssh/sshd_config
+  - sed -i -e '/^\(#\|\)PasswordAuthentication/s/^.*$/PasswordAuthentication no/' /etc/ssh/sshd_config
+  - sed -i -e '/^\(#\|\)KbdInteractiveAuthentication/s/^.*$/KbdInteractiveAuthentication no/' /etc/ssh/sshd_config
+  - sed -i -e '/^\(#\|\)ChallengeResponseAuthentication/s/^.*$/ChallengeResponseAuthentication no/' /etc/ssh/sshd_config
+  - sed -i -e '/^\(#\|\)MaxAuthTries/s/^.*$/MaxAuthTries 2/' /etc/ssh/sshd_config
+  - sed -i -e '/^\(#\|\)AllowTcpForwarding/s/^.*$/AllowTcpForwarding no/' /etc/ssh/sshd_config
+  - sed -i -e '/^\(#\|\)X11Forwarding/s/^.*$/X11Forwarding no/' /etc/ssh/sshd_config
+  - sed -i -e '/^\(#\|\)AllowAgentForwarding/s/^.*$/AllowAgentForwarding no/' /etc/ssh/sshd_config
+  - sed -i -e '/^\(#\|\)AuthorizedKeysFile/s/^.*$/AuthorizedKeysFile .ssh\/authorized_keys/' /etc/ssh/sshd_config
+  - sed -i '$a AllowUsers ${var.personal_user} ansible' /etc/ssh/sshd_config
   - >
     curl -L \
       -X POST \
