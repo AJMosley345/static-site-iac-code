a32eb76304e9e8bcf3f3f17de013871ee79b360b
ajmosley345 <ajmosley345@gmail.com>
2025-01-26 20:33:13 -0500

    Use id of server in cloud init

--- terraform/modules/compute/main.tf
+++ terraform/modules/compute/main.tf
@@ -15,26 +15,26 @@ resource "hcloud_ssh_key" "keys" {
 }
 
 # Takes the cloud config and maps the values to variables
-# data "template_file" "cloud_config" {
-#   template = file("${path.module}/cloud-init/cloud-config.yaml")
-#   vars = {
-#     ansible_user_ssh_key = var.ansible_user_ssh_key
-#     personal_user = var.personal_user
-#     tailscale_tailnet_key = var.tailscale_tailnet_key
-#     hcloud_api_token = var.hcloud_api_token
-#     server_id = hcloud_server.am_static_site.id
-#     github_pa_token = var.github_pa_token
-#     repo_name = var.repo_name
-#     workflow_id = var.workflow_id
-#   }
-# }
+data "template_file" "cloud_config" {
+  template = file("${path.module}/cloud-init/cloud-config.yaml")
+  vars = {
+    ansible_user_ssh_key = var.ansible_user_ssh_key
+    personal_user = var.personal_user
+    tailscale_tailnet_key = var.tailscale_tailnet_key
+    hcloud_api_token = var.hcloud_api_token
+    server_id = hcloud_server.am_static_site.id
+    github_pa_token = var.github_pa_token
+    repo_name = var.repo_name
+    workflow_id = var.workflow_id
+  }
+}
 
 resource "hcloud_server" "am_static_site" {
   name = var.server_name
   image = var.os_image
   server_type = var.server_type
   datacenter = var.datacenter
-  # user_data = data.template_file.cloud_config.rendered
+  user_data = data.template_file.cloud_config.rendered
   ssh_keys = [ for key in hcloud_ssh_key.keys : key.name  ]
   labels = {
     "role" : "webserver",
