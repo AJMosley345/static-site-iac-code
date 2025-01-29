a1a916ac8f258e27eac56c46af979f9c849bd2f9
ajmosley345 <ajmosley345@gmail.com>
2025-01-28 22:41:26 -0500

    Removed quick-setup and made sure yarn would be setup

--- quick-setup/main.tf
+++
@@ -1,8 +0,0 @@
-# Creates an an auth key at Tailscale and gives it a tag for servers
-data "hcloud_primary_ip" "primary_ip_id" {
-  name = "am-static-site"
-}
-
-output "primary_ip_id2" {
-  value = data.hcloud_primary_ip.primary_ip_id.id
-}
