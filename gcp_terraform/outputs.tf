65ab7173a826da4f911a1c48f733a73f87d48432
ajmosley345 <ajmosley345@gmail.com>
2025-02-03 18:57:44 -0500

    Forgot to comment out outputs

--- gcp_terraform/outputs.tf
+++ gcp_terraform/outputs.tf
@@ -1,15 +1,15 @@
-output "gcp_project_id" {
-  description = "Project ID for my GCP project"
-  value       = data.google_project.project_id.id
-}
+# output "gcp_project_id" {
+#   description = "Project ID for my GCP project"
+#   value       = data.google_project.project_id.id
+# }
 output "aws_route53_nameservers" {
   description = "Namservers of my Route 53 domain"
   value       = module.aws.aws_domain_zone_name_servers
 }
-output "gcp_static_ip" {
-  description = "Static ip of the server in GCP"
-  value       = module.gcp.static_ip
-}
+# output "gcp_static_ip" {
+#   description = "Static ip of the server in GCP"
+#   value       = module.gcp.static_ip
+# }
 output "tf_app_name" {
   value = var.tf_app_name
 }
